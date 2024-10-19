133575ae29c5f1f221bec9aa129141640208481a
NailClaros <40499772+NailClaros@users.noreply.github.com>
2024-10-19 18:05:35 -0400

    Update trans.py
    Updating trans.py to handle any size of text.

--- trans.py
+++ trans.py
@@ -27,27 +27,36 @@ def detect(text):
     language = ax['result']['language']
     return code, language
 
+
+
+
+
 def translate(text, lang):
-    url = "https://google-translate-api8.p.rapidapi.com/google-translate/"
+    if len(text) > 1000:
+        val = ""
+        for x in range(0, len(text), 1000): 
+            val+= translate(text[x:x + 1000], lang)
+    else:
+        url = "https://deep-translate1.p.rapidapi.com/language/translate/v2"
 
-    querystring = {"text":f"{text}", "lang":f"{lang}"}
+        payload = {
+            "q": str(text),
+            "source": "auto",
+            "target": str(lang)
+        }
+        headers = {
+            "x-rapidapi-key": key,
+            "x-rapidapi-host": "deep-translate1.p.rapidapi.com",
+            "Content-Type": "application/json"
+        }
 
-    payload = {
-        "key1": "value",
-        "key2": "value"
-    }
-    headers = {
-        "x-rapidapi-key": str(key),
-        "x-rapidapi-host": "google-translate-api8.p.rapidapi.com",
-        "Content-Type": "application/json"
-    }
+        response = requests.post(url, json=payload, headers=headers)
 
-    response = requests.post(url, json=payload, headers=headers, params=querystring)
-
-    print(response.json())
-    ax = json.loads(response.text)
-    
-    return ax['result']
+        print(response.json())
+        ax = json.loads(response.text)
+        
+        return ax['data']['translations']['translatedText']
+    return val
 
 ###Title
 ###Coverart
